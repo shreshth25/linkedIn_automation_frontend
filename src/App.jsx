abc8b10e6465e26c1d1abb63f36b4b70af01994b
Shreshth Bansal <shreshthbansal2505@gmail.com>
2024-02-15 15:17:15 +0530

    Code Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
